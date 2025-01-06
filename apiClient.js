cc19b3ebe1203ddae4a2f36861846e42d3931eae
tugaybakay <108657479+tugaybakay@users.noreply.github.com>
2025-01-07 02:14:00 +0300

    Update apiClient.js // http => https

--- apiClient.js
+++ apiClient.js
@@ -1,12 +1,12 @@
 export function fetchTimeZones() {
-  const baseURL = "http://worldtimeapi.org/api/timezone";
+  const baseURL = "https://worldtimeapi.org/api/timezone";
   return fetch(baseURL)
   .then(response => response.json())
 
 }
 
 export function fetchCurrentTime(timeZone) {
-  const baseURL = "http://worldtimeapi.org/api/timezone/";
+  const baseURL = "https://worldtimeapi.org/api/timezone/";
   return fetch(baseURL + timeZone)
   .then(response => response.json());
 }
